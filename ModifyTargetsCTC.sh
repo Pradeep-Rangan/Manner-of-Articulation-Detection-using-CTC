400fc35ae4b27e9d1712efa24c43fa6daa61d3d1
Pradeep-Rangan <pradeep_raj31@iitkgp.ac.in>
2018-10-30 13:45:48 +0530

    Add files via upload

---
+++ ModifyTargetsCTC.sh
@@ -0,0 +1,28 @@
+
+
+sed -i -- 's/A/#/g' txt/*
+sed -i -- 's/B/*/g' txt/*
+sed -i -- 's/C/*/g' txt/*
+sed -i -- 's/D/*/g' txt/*
+sed -i -- 's/E/#/g' txt/*
+sed -i -- 's/F/%/g' txt/*
+sed -i -- 's/G/*/g' txt/*
+sed -i -- 's/H/%/g' txt/*
+sed -i -- 's/I/#/g' txt/*
+sed -i -- 's/J/%/g' txt/*
+sed -i -- 's/K/*/g' txt/*
+sed -i -- 's/L/$/g' txt/*
+sed -i -- 's/M/@/g' txt/*
+sed -i -- 's/N/@/g' txt/*
+sed -i -- 's/O/#/g' txt/*
+sed -i -- 's/P/*/g' txt/*
+sed -i -- 's/Q/*/g' txt/*
+sed -i -- 's/R/$/g' txt/*
+sed -i -- 's/S/%/g' txt/*
+sed -i -- 's/T/*/g' txt/*
+sed -i -- 's/U/#/g' txt/*
+sed -i -- 's/V/%/g' txt/*
+sed -i -- 's/W/$/g' txt/*
+sed -i -- 's/X/%/g' txt/*
+sed -i -- 's/Y/$/g' txt/*
+sed -i -- 's/Z/%/g' txt/*
