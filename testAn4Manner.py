ca5f1a19f1078e16ce5835755199c0f54a743776
Pradeep-Rangan <pradeep_raj31@iitkgp.ac.in>
2018-10-30 11:46:17 +0530

    Add files via upload

---
+++ testAn4Manner.py
@@ -0,0 +1,100 @@
+import argparse
+
+import numpy as np
+import torch
+from tqdm import tqdm
+
+from data.data_loader import SpectrogramDataset, AudioDataLoader
+from decoder import GreedyDecoder
+from model import DeepSpeech
+from opts import add_decoder_args, add_inference_args
+
+parser = argparse.ArgumentParser(description='DeepSpeech transcription')
+parser = add_inference_args(parser)
+parser.add_argument('--test-manifest', metavar='DIR',
+                    help='path to validation manifest csv', default='data/test_manifest.csv')
+parser.add_argument('--batch-size', default=20, type=int, help='Batch size for training')
+parser.add_argument('--num-workers', default=4, type=int, help='Number of workers used in dataloading')
+parser.add_argument('--verbose', action="store_true", help="print out decoded output and error of each sample")
+no_decoder_args = parser.add_argument_group("No Decoder Options", "Configuration options for when no decoder is "
+                                                                  "specified")
+no_decoder_args.add_argument('--output-path', default=None, type=str, help="Where to save raw acoustic output")
+parser = add_decoder_args(parser)
+args = parser.parse_args()
+args.model_path = 'modelsAn4Manner/deepspeech_final.pth'
+args.test_manifest='data/an4_valManner_manifest.csv'
+
+
+if __name__ == '__main__':
+    torch.set_grad_enabled(False)
+    model = DeepSpeech.load_model(args.model_path)
+    if args.cuda:
+        model.cuda()
+    model.eval()
+
+    labels = DeepSpeech.get_labels(model)
+    audio_conf = DeepSpeech.get_audio_conf(model)
+
+    if args.decoder == "beam":
+        from decoder import BeamCTCDecoder
+
+        decoder = BeamCTCDecoder(labels, lm_path=args.lm_path, alpha=args.alpha, beta=args.beta,
+                                 cutoff_top_n=args.cutoff_top_n, cutoff_prob=args.cutoff_prob,
+                                 beam_width=args.beam_width, num_processes=args.lm_workers)
+    elif args.decoder == "greedy":
+        decoder = GreedyDecoder(labels, blank_index=labels.index('_'))
+    else:
+        decoder = None
+    target_decoder = GreedyDecoder(labels, blank_index=labels.index('_'))
+    test_dataset = SpectrogramDataset(audio_conf=audio_conf, manifest_filepath=args.test_manifest, labels=labels,
+                                      normalize=True)
+    test_loader = AudioDataLoader(test_dataset, batch_size=args.batch_size,
+                                  num_workers=args.num_workers)
+    total_cer, total_wer, num_tokens, num_chars = 0, 0, 0, 0
+    output_data = []
+    for i, (data) in tqdm(enumerate(test_loader), total=len(test_loader)):
+        inputs, targets, input_percentages, target_sizes = data
+        input_sizes = input_percentages.mul_(int(inputs.size(3))).int()
+
+        # unflatten targets
+        split_targets = []
+        offset = 0
+        for size in target_sizes:
+            split_targets.append(targets[offset:offset + size])
+            offset += size
+
+        if args.cuda:
+            inputs = inputs.cuda()
+
+        out, output_sizes = model(inputs, input_sizes)
+
+        if decoder is None:
+            # add output to data array, and continue
+            output_data.append((out.numpy(), output_sizes.numpy()))
+            continue
+
+        decoded_output, _ = decoder.decode(out.data, output_sizes.data)
+        target_strings = target_decoder.convert_to_strings(split_targets)
+        for x in range(len(target_strings)):
+            transcript, reference = decoded_output[x][0], target_strings[x][0]
+            wer_inst = decoder.wer(transcript, reference)
+            cer_inst = decoder.cer(transcript, reference)
+            total_wer += wer_inst
+            total_cer += cer_inst
+            num_tokens += len(reference.split())
+            num_chars += len(reference)
+            if args.verbose:
+                print("Utterance ID:", test_dataset.ids[x])
+                print("Ref:", reference.lower())
+                print("Hyp:", transcript.lower())
+                print("WER:", float(wer_inst) / len(reference.split()), "CER:", float(cer_inst) / len(reference), "\n")
+
+    if decoder is not None:
+        wer = float(total_wer) / num_tokens
+        cer = float(total_cer) / num_chars
+
+        print('Test Summary \t'
+              'Average WER {wer:.3f}\t'
+              'Average CER {cer:.3f}\t'.format(wer=wer * 100, cer=cer * 100))
+    else:
+        np.save(args.output_path, output_data)
